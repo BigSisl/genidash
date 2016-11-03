bb785a8203829e21248d7b3e9531071607bc1c23
Lucien Zuercher <l.zuercher@tincan.ch>
2016-11-03 15:26:12 +0100

    chore: prepare browerify for compiling system

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * will start a test server / files can be profideed by any kind of system.
+ * just needs location:
+ * - components_templates/ -> html sources
+ * - components_scripts/ -> js sources
+ * - pages/ -> json source
+ * - user_config/
+ */
